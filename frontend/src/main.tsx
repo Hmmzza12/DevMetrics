5199e6e9ffff8735eb4b4f21a6998c490b5db0c5
Hmmzza12 <hamzatouaban11@gmail.com>
2026-08-13 16:02:51 +0100

    Remove TanStack Query and Router devtools

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { RouterProvider, createRouter } from '@tanstack/react-router';
 import './index.css';
 import { queryClient } from '@/lib/query-client';
@@ -22,7 +21,6 @@ createRoot(document.getElementById('root')!).render(
       <LenisProvider>
         <RouterProvider router={router} />
       </LenisProvider>
-      {import.meta.env.DEV && <ReactQueryDevtools initialIsOpen={false} />}
     </QueryClientProvider>
   </StrictMode>,
 );
--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -1,15 +1,9 @@
 import { createRootRoute, Outlet } from '@tanstack/react-router';
-import { TanStackRouterDevtools } from '@tanstack/react-router-devtools';
 
 export const Route = createRootRoute({
   component: RootComponent,
 });
 
 function RootComponent() {
-  return (
-    <>
-      <Outlet />
-      {import.meta.env.DEV && <TanStackRouterDevtools position="bottom-right" />}
-    </>
-  );
+  return <Outlet />;
 }
